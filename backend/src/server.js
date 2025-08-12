0194e47bfc01a6d7a38508a3c3e907cdccd7bc08
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-08-12 23:09:25 +0300

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import chatRoutes from "./routes/chat.route.js"
+import {connectDB} from "./lib/db.js"
+import cookieParser from "cookie-parser";
+
+
+const app = express()
+const PORT = process.env.PORT || 5001
+
+// CORS middleware
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Vite default port
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/chat", chatRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+  connectDB();
+});
